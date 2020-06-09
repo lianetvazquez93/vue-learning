4738810dc764baf5a95dbeba0e174550bf508e04
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-06-09 11:21:44 +0200

    Event handling

--- product-page/main.js
+++ product-page/main.js
@@ -2,19 +2,34 @@ const app = new Vue({
     el: "#app",
     data: {
         product: "Socks",
-        image: "./assets/vmSocks-green-onWhite.jpg",
+        image: "./assets/vmSocks-green.jpg",
         inStock: true,
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
         variants: [
             {
                 variantId: 2234,
-                variantColor: "green"
+                variantColor: "green",
+                variantImage: "./assets/vmSocks-green.jpg"
             },
             {
                 variantId: 2235,
-                variantColor: "blue"
+                variantColor: "blue",
+                variantImage: "./assets/vmSocks-blue.jpg"
             }
         ],
-        sizes: ["35-38", "39-42", "43-46"]
+        cart: 0
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage;
+        },
+        decrementCart() {
+            if (this.cart > 0) {
+                this.cart -= 1;
+            }
+        }
     }
 });
