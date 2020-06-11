fd8b979c63073bb9ccba0178dd182a82b6ec0102
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-06-11 15:35:47 +0200

    Vue router

---
+++ real-world-vue/src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import EventCreate from "../views/EventCreate";
+import EventList from "../views/EventList";
+import EventShow from "../views/EventShow";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "event-list",
+    component: EventList
+  },
+  {
+    path: "/event",
+    name: "event-show",
+    component: EventShow
+  },
+  {
+    path: "/event/create",
+    name: "event-create",
+    component: EventCreate
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
