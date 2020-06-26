0719d14fcea08656ef30edb7c468d917121b54b5
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-06-26 11:26:05 +0200

    feat: create board component

---
+++ trello-clone/src/views/Board.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="board">
+    <div class="flex flex-row items-start">
+      <div class="column" v-for="column of board.columns" :key="column.name">
+        <div class="flex items-center mb-2 font-bold">
+          {{ column.name }}
+        </div>
+        <div class="list-reset">
+          <div class="task" v-for="task of column.tasks" :key="task.id">
+            <span class="w-full flex-no-shrink font-bold">
+              {{ task.name }}
+            </span>
+            <p
+              class="w-full flex-no-shrink mt-1 text-sm"
+              v-if="task.description"
+            >
+              {{ task.description }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  computed: mapState(["board"]),
+};
+</script>
+
+<style lang="postcss">
+.task {
+  @apply flex items-center flex-wrap shadow mb-2 py-2 px-2 no-underline bg-white rounded;
+}
+
+.column {
+  @apply bg-gray-400 p-2 mr-4 shadow rounded;
+  min-width: 350px;
+}
+.board {
+  @apply p-4 bg-teal-600 h-full overflow-auto;
+}
+.task-bg {
+  @apply inset-0 absolute;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+</style>
