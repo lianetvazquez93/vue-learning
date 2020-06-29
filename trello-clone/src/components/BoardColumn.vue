104d1238be752b67091b9cd69eb3237121013653
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-06-29 19:54:33 +0200

    refactor: create a mixin for moving tasks and columns

--- trello-clone/src/components/BoardColumn.vue
+++ trello-clone/src/components/BoardColumn.vue
@@ -32,24 +32,13 @@
 </template>
 
 <script>
+import movingTasksAndColumnsMixin from "../mixins/movingTasksAndColumnsMixin";
+
 export default {
   name: "board-column",
+  mixins: [movingTasksAndColumnsMixin],
   beforeCreate: function() {
     this.$options.components.ColumnTask = require("./ColumnTask").default;
-  },
-  props: {
-    column: {
-      type: Object,
-      required: true,
-    },
-    columnIndex: {
-      type: Number,
-      required: true,
-    },
-    board: {
-      type: Object,
-      required: true,
-    },
   },
   methods: {
     createTask(event, tasks) {
@@ -65,38 +54,6 @@ export default {
 
       event.dataTransfer.setData("from-column-index", fromColumnIdex);
       event.dataTransfer.setData("type", "column");
-    },
-    moveTaskOrColumn(event, toTasks, toColumnIndex, toTaskIndex) {
-      const type = event.dataTransfer.getData("type");
-      if (type === "task") {
-        this.moveTask(
-          event,
-          toTasks,
-          toTaskIndex !== undefined ? toTaskIndex : toTasks.length,
-        );
-      } else {
-        this.moveColumn(event, toColumnIndex);
-      }
-    },
-    moveTask(event, toTasks, toTaskIndex) {
-      const fromColumnIndex = event.dataTransfer.getData("from-column-index");
-      const fromTasks = this.board.columns[fromColumnIndex].tasks;
-      const fromTaskIndex = event.dataTransfer.getData("from-task-index");
-
-      this.$store.commit("MOVE_TASK", {
-        fromTasks,
-        fromTaskIndex,
-        toTasks,
-        toTaskIndex,
-      });
-    },
-    moveColumn(event, toColumnIndex) {
-      const fromColumnIndex = event.dataTransfer.getData("from-column-index");
-
-      this.$store.commit("MOVE_COLUMN", {
-        fromColumnIndex,
-        toColumnIndex,
-      });
     },
   },
 };
--- trello-clone/src/components/ColumnTask.vue
+++ trello-clone/src/components/ColumnTask.vue
@@ -18,8 +18,11 @@
 </template>
 
 <script>
+import movingTasksAndColumnsMixin from "../mixins/movingTasksAndColumnsMixin";
+
 export default {
   name: "column-task",
+  mixins: [movingTasksAndColumnsMixin],
   props: {
     task: {
       type: Object,
@@ -27,18 +30,6 @@ export default {
     },
     taskIndex: {
       type: Number,
-      required: true,
-    },
-    column: {
-      type: Object,
-      required: true,
-    },
-    columnIndex: {
-      type: Number,
-      required: true,
-    },
-    board: {
-      type: Object,
       required: true,
     },
   },
@@ -53,38 +44,6 @@ export default {
       event.dataTransfer.setData("from-task-index", taskIndex);
       event.dataTransfer.setData("from-column-index", fromColumnIndex);
       event.dataTransfer.setData("type", "task");
-    },
-    moveTaskOrColumn(event, toTasks, toColumnIndex, toTaskIndex) {
-      const type = event.dataTransfer.getData("type");
-      if (type === "task") {
-        this.moveTask(
-          event,
-          toTasks,
-          toTaskIndex !== undefined ? toTaskIndex : toTasks.length,
-        );
-      } else {
-        this.moveColumn(event, toColumnIndex);
-      }
-    },
-    moveTask(event, toTasks, toTaskIndex) {
-      const fromColumnIndex = event.dataTransfer.getData("from-column-index");
-      const fromTasks = this.board.columns[fromColumnIndex].tasks;
-      const fromTaskIndex = event.dataTransfer.getData("from-task-index");
-
-      this.$store.commit("MOVE_TASK", {
-        fromTasks,
-        fromTaskIndex,
-        toTasks,
-        toTaskIndex,
-      });
-    },
-    moveColumn(event, toColumnIndex) {
-      const fromColumnIndex = event.dataTransfer.getData("from-column-index");
-
-      this.$store.commit("MOVE_COLUMN", {
-        fromColumnIndex,
-        toColumnIndex,
-      });
     },
   },
 };
